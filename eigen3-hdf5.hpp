3cdf32998f46e61e499344c1c1e005b90d165cb3
Philip Miller <philip.miller@sri.com>
2015-10-08 02:31:30 -0400

    improvements for loading into colmajor matrix
    added method read_colmat that will read a dataset directly into the
    memory for a column major matrix. It does so by using a hyperslab
    to read a row at a time

--- eigen3-hdf5.hpp
+++ eigen3-hdf5.hpp
@@ -376,6 +376,61 @@ namespace internal
     {
         dataset.read(datatype, data);
     }
+    
+    template <typename Derived>
+    bool read_colmat(Eigen::EigenBase<Derived>* mat,
+        const H5::DataType * const datatype,
+        const H5::DataSet &dataset)
+    {
+        if (mat->derived().innerStride() != 1)
+        {
+            // inner stride != 1 is an edge case this function does not (yet) handle. (I think it
+            // could by using the inner stride as the first element of mstride below. But I do
+            // not have a test case to try it out, so just return false for now.) 
+            return false;
+        }
+
+        Derived::Index rows = mat->rows();
+        Derived::Index cols = mat->cols();
+        Derived::Index stride = mat->derived().outerStride();
+        if (stride != rows)
+        {
+            // this function does not (yet) read into a mat that has a different stride than the
+            // dataset. 
+            return false;
+        }
+
+
+        // slab params for the file data
+        hsize_t fstride[2] = { 1, cols };
+        hsize_t fcount[2] = { 1, 1 };
+        hsize_t fblock[2] = { 1, cols };
+
+        // file dataspace
+        hsize_t fdim[2] = { rows, cols };
+        H5::DataSpace fspace(2, fdim);
+
+        // slab params for the memory data
+        hsize_t mstride[2] = { stride, 1 };
+        hsize_t mcount[2] = { 1, 1 };
+        hsize_t mblock[2] = { cols, 1 };
+
+        // memory dataspace
+        hsize_t mdim[2] = { cols, stride };
+        H5::DataSpace mspace(2, mdim);
+
+        // transpose the column major data in memory to the row major data in the file by
+        // writing one row slab at a time. 
+        for (int i = 0; i < rows; i++)
+        {
+            hsize_t fstart[2] = { i, 0 };
+            hsize_t mstart[2] = { 0, i };
+            fspace.selectHyperslab(H5S_SELECT_SET, fcount, fstart, fstride, fblock);
+            mspace.selectHyperslab(H5S_SELECT_SET, mcount, mstart, mstride, mblock);
+            dataset.read(mat->derived().data(), *datatype, mspace, fspace);
+        }
+        return true;
+    }
 
     template <typename Derived, typename DataSet>
     void _load (const DataSet &dataset, const Eigen::DenseBase<Derived> &mat)
@@ -394,23 +449,32 @@ namespace internal
         const hsize_t rows = dimensions[0], cols = dimensions[1];
         const H5::DataType * const datatype = DatatypeSpecialization<Scalar>::get();
         Eigen::DenseBase<Derived> &mat_ = const_cast<Eigen::DenseBase<Derived> &>(mat);
+        mat_.derived().resize(rows, cols);
         bool written = false;
         if (mat.Flags & Eigen::RowMajor || dimensions[0] == 1 || dimensions[1] == 1)
         {
-            // mat is already row major; resize it and read into it
-            mat_.derived().resize(rows, cols);
+            // mat is already row major
             Derived::Index stride = mat_.derived().outerStride();
             if (stride == cols || (stride == rows && cols == 1))
             {
                 // mat has natural stride, so read directly into its data block
-                internal::read_data(dataset, mat_.derived().data(), *datatype);
+                read_data(dataset, mat_.derived().data(), *datatype);
                 written = true;
             }
+        }
+        else 
+        {
+            // colmajor flag is 0 so the assert needs to check that mat is not rowmajor. 
+            assert(!(mat.Flags & Eigen::RowMajor));
+
+            written = read_colmat(&mat_, datatype, dataset);
         }
 
         if (!written)
         {
-            // input is col major or has unnatural stride; read into a temp and copy it
+            // dataset has not been loaded directly into mat_, so as a last resort read it into a
+            // temp and copy it to mat_. (Should only need to do this when the mat_ to be loaded
+            // into has an unnatural stride.) 
             Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> temp(rows, cols);
             internal::read_data(dataset, temp.data(), *datatype);
             mat_ = temp;
